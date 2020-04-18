e6cbe49cd448c179bfb92d1a3f261b8472deefc9
Codain <romain2boss@yahoo.fr>
2020-04-18 22:45:15 +0200

    Add first test units

---
+++ tests/API/NominatimTest.php
@@ -0,0 +1,38 @@
+<?php
+	declare(strict_types=1);
+	
+	require_once __DIR__.'/../../src/API/Nominatim.php';
+	require_once __DIR__.'/../../src/Exception.php';
+	
+	use PHPUnit\Framework\TestCase;
+
+	final class Thrush_NominatimTest extends TestCase
+	{
+		public function testCanBeCreatedFromValidEmailAddress(): void
+		{
+			$cache = new Thrush_Cache('localhost', 'localhost', './cache/');
+			
+			$this->assertInstanceOf(
+				Thrush_Nominatim::class,
+				new Thrush_Nominatim($cache, 'user@example.com')
+			);
+		}
+		
+		public function testCannotBeCreatedFromInvalidEmailAddress(): void
+		{
+			$cache = new Thrush_Cache('localhost', 'localhost', './cache/');
+			
+			$this->expectException(Thrush_Exception::class);
+
+			new Thrush_Nominatim($cache, 'invalid');
+		}
+		
+		/*public function testCanBeUsedAsString(): void
+		{
+			$this->assertEquals(
+				'user@example.com',
+				Email::fromString('user@example.com')
+			);
+		}*/
+	}
+?>
